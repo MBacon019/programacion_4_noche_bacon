2befa4aa4f2ed7ddc2a6f343c3b8fa8aeaa1c8b6
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-04-07 20:06:28 -0500

    Clase 4-07-2026

---
+++ modulo_1/basics_new/02_variables.kt
@@ -0,0 +1,37 @@
+fun main() {
+    val nombre = "Heymi"
+    val edad: Int = 20
+    var contador = 0 
+    contador = contador + 1
+    
+    println("$nombre tiene $edad años")
+
+    val n1: Byte = 127
+    val n2: Short = 32_767
+    val n3: Int = 12
+    val n4: Long = 2_222_222_222_2L 
+
+    println(n1)
+    println(n2)
+    println(n3)
+    println(n4)
+
+    val boleano: Boolean = true
+    val caracter: Char = 'k'
+    val cadena: String = " Kotlin"
+
+    val inferidoString = "String"
+    println("Tipo: ${inferidoString::class.simpleName}")
+    
+    val inferidoInt = 12
+    println("Tipo: ${inferidoInt::class.simpleName}")
+
+    val primerNombre = "Alex"
+    val primerApellido = "Perez" // Corregido: "primerapellido" -> "primerApellido"
+    
+    val primerNombreMayuscula = primerNombre.uppercase()
+    val primerApellidoMayuscula = primerApellido.uppercase()
+
+    println("Nombre completo: ${primerNombreMayuscula}${primerApellidoMayuscula}")
+    println("Nombre completo: ${primerNombre.uppercase()}${primerApellido.uppercase()}")
+}
---
+++ modulo_1/basics_new/03_operadores_aritmeticos.kt
@@ -0,0 +1,35 @@
+
+    // --- Operadores aritmeticos
+
+
+fun main() {
+    
+    val numero1 = 10
+    val numero2= 2
+    
+    println("Suma")
+    
+    println(numero1, "+", numero2, "=", numero1-numero2)
+    println("Resta")
+	println(numero1, "-", numero2, "=", numero1-numero2)
+    println(numero1, "+", numero2, "=", numero1-numero2)
+    println(numero1, "+", numero2, "=", numero1-numero2)
+
+
+  
+fun main() {
+    val numero1 = 10
+    val numero2 = 2
+        
+    // Suma
+    println("$numero1 + $numero2 = ${numero1 + numero2}")
+    
+    // Resta
+    println("$numero1 - $numero2 = ${numero1 - numero2}")
+    
+    // Multiplicación
+    println("$numero1 * $numero2 = ${numero1 * numero2}")
+    
+    // División
+    println("$numero1 / $numero2 = ${numero1 / numero2}")
+}
